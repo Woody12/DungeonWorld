f6756f02c6874a424f34f531547a25176e2018aa
Wuthichai Lee <wulee12@gmail.com>
2020-09-19 23:32:31 -0700

    Add Unity Dungeon Scenes

---
+++ Assets/Plugins/signin.bat
@@ -0,0 +1,2 @@
+title Sign In to AltspaceVR
+curl -v -d "user[email]=wolee_2000@yahoo.com&user[password]=Leelaluck12@" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/wuthichailee/Documents/Unity/AltSpace World/BakedLightDungeon/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1565717525163934697.json
